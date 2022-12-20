69a1ccc42c14a881efc7141464d1e61d98419ccd
leopard4 <95200973+leopard4@users.noreply.github.com>
2022-12-20 11:32:30 +0900

    Update app_eda.py

--- app_eda.py
+++ app_eda.py
@@ -4,19 +4,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sb
 
-import platform
-
-from matplotlib import font_manager, rc
-plt.rcParams['axes.unicode_minus'] = False
-
-if platform.system() == 'Darwin':
-    rc('font', family='AppleGothic')
-elif platform.system() == 'Windows':
-    path = "c:/Windows/Fonts/malgun.ttf"
-    font_name = font_manager.FontProperties(fname=path).get_name()
-    rc('font', family=font_name)
-else:
-    print('Unknown system... sorry~~~~')
 
 def run_eda_app() : 
     df = pd.read_csv('data/Car_Purchasing_Data.csv',encoding= 'iso-8859-1')
